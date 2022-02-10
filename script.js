3232c8e445a7433253146d22617be9e3f46945b4
pybin94 <73331322+pybin94@users.noreply.github.com>
2022-02-10 22:40:36 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const margin = [10000,15000,30000,45000,75000,120000,195000,315000,410000,725000];
 
-let list = []
+let list = [];
+let gameResult = [];
+
+let playerScoreList = [];
+let bankerScoreList = [];
+
+let playerLoseStreakList = [];
+let bankerLoseStreakList = [];
+
+let playerMultiLoseList = [];
+let bankerMultiLoseList = [];
+
+let playerpriceList = [];
+let bankerpriceList = [];
+
+let winCount = 0;
+let loseCount = 0;
+let winRateCheck = 0;
 
 let limit = 10;
 
@@ -17,32 +34,75 @@ let playerprice = 0;
 let bankerprice = 0;
 
 
-const bettingScreen = document.querySelector("#bettingScreen")
+const bettingScreen = document.querySelector("#bettingScreen");
 
-const BTN_Player = document.querySelector("#player")
-const BTN_Banker = document.querySelector("#banker")
+const BTN_Player = document.querySelector("#player");
+const BTN_Banker = document.querySelector("#banker");
+const win = document.querySelector("#win");
+const lose = document.querySelector("#lose");
+const winRate = document.querySelector("#winRate");
+
+function gameReset(){
+
+    bettingScreen.innerHTML = "게임 결과를 입력해주세요.";
+    bettingScreen.style.borderColor = "#c7c7c7";
+    bettingScreen.style.boxShadow = "0 0 0 1px #c7c7c7 inset";
+    bettingScreen.style.color = "#000";
+    playerLoseStreak = false;
+    bankerLoseStreak = false;
+    playerMultiLose = false;
+    bankerMultiLose = false;
+    playerScore = 0;
+    bankerScore = 0;
+    winCount = 0;
+    loseCount = 0;
+    winRateCheck = 0;
+    winRate.innerHTML = 0;
+    win.innerHTML = 0;
+    lose.innerHTML = 0;
+    list = []
+
+}
+
+document.querySelector("#resetBtn").addEventListener("click", function(){
+    gameReset();
+})
 
 BTN_Player.addEventListener("click", function(){
 
+    // 승률
+    if(document.querySelector("#bettingScreen").innerText.includes("플레이어") === true){
+
+        winCount++;
+        win.innerHTML = winCount;
+        winRate.innerHTML = Math.floor((winCount/(winCount+loseCount))*100);
+
+    } else if (document.querySelector("#bettingScreen").innerText.includes("뱅커") === true){
+
+        loseCount++;
+        lose.innerHTML = loseCount;
+        winRate.innerHTML = Math.floor((winCount/(winCount+loseCount))*100);
+
+    } 
 
 
     if(list.slice(-2)[0] === 'B' && list.slice(-2)[1] === 'B'){     // 2연패
 
         playerLoseStreak = true;
         bankerScore++;
-        console.log("플레이어 A")
+        console.log("플레이어 A");
 
     } else if ( list.slice(-2)[0] === 'A' && list.slice(-2)[1] === 'A' ){  
 
         if ( playerMultiLose === true ){
 
             playerMultiLose === false;
-            onsole.log("플레이어 B-1")
+            onsole.log("플레이어 B-1");
 
         } else {
 
             playerScore -= 2;
-            console.log("플레이어 B-2")
+            console.log("플레이어 B-2");
 
         }
 
@@ -53,12 +113,12 @@ BTN_Player.addEventListener("click", function(){
             bankerScore++;
             playerLoseStreak = false;
             playerMultiLose = false;
-            console.log("플레이어 C-1")
+            console.log("플레이어 C-1");
 
         } else {
 
             bankerScore++;
-            console.log("플레이어 C-2")
+            console.log("플레이어 C-2");
 
         }
 
@@ -66,40 +126,47 @@ BTN_Player.addEventListener("click", function(){
     } 
     else if (bankerLoseStreak === true && list.slice(-2)[0] === 'B' && list.slice(-2)[1] === 'A'){ // 뱅커 연패
 
-        bankerScore++;
-        playerScore -= 2;
-        bankerLoseStreak = false;
-        bankerMultiLose = true;
-        console.log("플레이어 D")
+        if(bankerMultiLose === true){
+            playerScore -= 2;
+            console.log("플레이어 D-1");
+
+        } else {
+            bankerScore++;
+            playerScore -= 2;
+            bankerLoseStreak = false;
+            bankerMultiLose = true;
+            console.log("플레이어 D-2");
+        }
+
 
     } else if (bankerMultiLose === true){      //ppbppbp
 
         playerScore -= 2;
-        console.log("플레이어 E")
+        console.log("플레이어 E");
 
     } else {                          // 일반 클릭
 
         playerScore -= 2;
         bankerScore++;
         playerLoseStreak = false;
-        console.log("플레이어 F")
+        console.log("플레이어 F");
 
     }
 
     if (playerScore <= 0) {
-        playerScore = 0
+        playerScore = 0;
     }
 
-    var playerPrice = margin[playerScore]
-    var bankerPrice = margin[bankerScore]
+    var playerPrice = margin[playerScore];
+    var bankerPrice = margin[bankerScore];
 
     if ( list.slice(-2)[0] === 'B' && list.slice(-2)[1] === 'B' && bankerLoseStreak === true ){ 
         bankerPrice = 0;
     } 
 
 
-    list.push("A")
-    console.log(list)
+    list.push("A");
+    console.log(list);
 
     if( list.slice(-2)[0] === 'A' && list.slice(-2)[1] === 'A' ){
         bankerPrice = 0;
@@ -113,43 +180,68 @@ BTN_Player.addEventListener("click", function(){
         playerPrice = 0;
     }
 
-    console.log("플레이어 연패 " + playerLoseStreak)
-    console.log("뱅커 연패 "+bankerLoseStreak)
-    console.log("플레이어 4연패 " + playerMultiLose)
-    console.log("뱅커 4연패 "+bankerMultiLose)
-    console.log("뱅커 "+bankerPrice)
-    console.log("플레이어 "+playerPrice)
+    console.log("플레이어 연패 " + playerLoseStreak);
+    console.log("뱅커 연패 "+bankerLoseStreak);
+    console.log("플레이어 4연패 " + playerMultiLose);
+    console.log("뱅커 4연패 "+bankerMultiLose);
+    console.log("뱅커 "+bankerPrice);
+    console.log("플레이어 "+playerPrice);
     
     const culBoth = playerPrice - bankerPrice;
 
     if(culBoth > 0) {
+
         bettingScreen.innerHTML = "플레이어 " + Math.abs(culBoth);
-        bettingScreen.style.borderColor = "#3498db"
+        bettingScreen.style.borderColor = "#3498db";
+        bettingScreen.style.boxShadow = "0 0 0 1px #3498db inset";
+        bettingScreen.style.color = "#3498db";
 
     }else if (culBoth < 0){
+
         bettingScreen.innerHTML = "뱅커 " + Math.abs(culBoth);
-        bettingScreen.style.borderColor = "#e74c3c"
+        bettingScreen.style.borderColor = "#e74c3c";
+        bettingScreen.style.boxShadow = "0 0 0 1px #e74c3c inset";
+        bettingScreen.style.color = "#e74c3c";
 
     }else if (culBoth === 0){
+
         bettingScreen.innerHTML = "휴식";
+        bettingScreen.style.borderColor = "#c7c7c7";
+        bettingScreen.style.boxShadow = "0 0 0 1px #c7c7c7 inset";
+        bettingScreen.style.color = "#c7c7c7";
 
     }
 
     if(bankerScore >= limit){
-        bettingScreen.innerHTML = "배팅금지";
+
+        alert( "마틴에 도달했습니다. 게임을 다시 시작합니다." );
+        gameReset();
+
     }
 
 
 
-    // document.querySelector("#playerScore").innerHTML = playerScore;
-    // document.querySelector("#bankerScore").innerHTML = bankerScore;
+    document.querySelector("#playerScore").innerHTML = playerScore;
+    document.querySelector("#bankerScore").innerHTML = bankerScore;
 
 })
 
 BTN_Banker.addEventListener("click", function(){
 
+    // 승률
+    if(document.querySelector("#bettingScreen").innerText.includes("뱅커") === true){
 
+        winCount++;
+        win.innerHTML = winCount;
+        winRate.innerHTML = Math.floor((winCount/(winCount+loseCount))*100);
 
+    } else if (document.querySelector("#bettingScreen").innerText.includes("플레이어") === true) {
+
+        loseCount++;
+        lose.innerHTML = loseCount;
+        winRate.innerHTML = Math.floor((winCount/(winCount+loseCount))*100);
+
+    }
 
     if(list.slice(-2)[0] === 'A' && list.slice(-2)[1] === 'A'){     // 2연패
 
@@ -162,12 +254,12 @@ BTN_Banker.addEventListener("click", function(){
         if ( bankerMultiLose === true ){
 
             bankerMultiLose === false;
-            console.log("뱅커 B-1")
+            console.log("뱅커 B-1");
 
         } else {
 
             bankerScore -= 2;
-            console.log("뱅커 B-2")
+            console.log("뱅커 B-2");
 
         }
 
@@ -179,52 +271,59 @@ BTN_Banker.addEventListener("click", function(){
             playerScore++;
             bankerLoseStreak = false;
             bankerMultiLose = false;
-            console.log("뱅커 C-1")
+            console.log("뱅커 C-1");
 
         } else {
 
             playerScore++;
-            console.log("뱅커 C-2")
+            console.log("뱅커 C-2");
 
         }
 
 
     } else if ( playerLoseStreak === true && list.slice(-2)[0] === 'A' && list.slice(-2)[1] === 'B'){
 
-        playerScore++;
-        bankerScore -= 2;
-        playerLoseStreak = false;
-        playerMultiLose = true;
-        console.log("뱅커 D")
+        if(playerMultiLose === true){
+            bankerScore -= 2;
+            console.log("뱅커 D-1");
+
+        } else {
+            playerScore++;
+            bankerScore -= 2;
+            playerLoseStreak = false;
+            playerMultiLose = true;
+            console.log("뱅커 D-2");
+        }
+
 
     } else if (playerMultiLose === true){      //ppbppbp
 
         bankerScore -= 2;
-        console.log("뱅커 E")
+        console.log("뱅커 E");
 
     } else {
 
         bankerScore -= 2;
         playerScore++;
         bankerLoseStreak = false;
-        console.log("뱅커 F")
+        console.log("뱅커 F");
 
     }
 
     if (bankerScore <= 0) {
-        bankerScore = 0
+        bankerScore = 0;
     }
     
-    let playerPrice = margin[playerScore]
-    let bankerPrice = margin[bankerScore]
+    let playerPrice = margin[playerScore];
+    let bankerPrice = margin[bankerScore];
 
     if ( list.slice(-2)[0] === 'A' && list.slice(-2)[1] === 'A' && playerLoseStreak === true){ 
         playerPrice = 0;
 
     } 
 
-    list.push("B")
-    console.log(list)
+    list.push("B");
+    console.log(list);
 
     if( list.slice(-2)[0] === 'B' && list.slice(-2)[1] === 'B'){
         playerPrice = 0;
@@ -239,33 +338,73 @@ BTN_Banker.addEventListener("click", function(){
 
     }
 
-    console.log("플레이어 연패 " + playerLoseStreak)
-    console.log("뱅커 연패 "+bankerLoseStreak)
-    console.log("플레이어 4연패 " + playerMultiLose)
-    console.log("뱅커 4연패 "+bankerMultiLose)
-    console.log("뱅커 "+bankerPrice)
-    console.log("플레이어 "+playerPrice)
+    console.log("플레이어 연패 " + playerLoseStreak);
+    console.log("뱅커 연패 "+bankerLoseStreak);
+    console.log("플레이어 4연패 " + playerMultiLose);
+    console.log("뱅커 4연패 "+bankerMultiLose);
+    console.log("뱅커 "+bankerPrice);
+    console.log("플레이어 "+playerPrice);
 
     const culBoth = playerPrice - bankerPrice;
 
+
+
+
     if(culBoth > 0) {
+
         bettingScreen.innerHTML = "플레이어 " + Math.abs(culBoth);
-        bettingScreen.style.borderColor = "#3498db"
+        bettingScreen.style.borderColor = "#3498db";
+        bettingScreen.style.boxShadow = "0 0 0 1px #3498db inset";
+        bettingScreen.style.color = "#3498db";
 
     }else if (culBoth < 0){
+
         bettingScreen.innerHTML = "뱅커 " + Math.abs(culBoth);
-        bettingScreen.style.borderColor = "#e74c3c"
+        bettingScreen.style.borderColor = "#e74c3c";
+        bettingScreen.style.boxShadow = "0 0 0 1px #e74c3c inset";
+        bettingScreen.style.color = "#e74c3c";
 
     }else if (culBoth === 0){
+
         bettingScreen.innerHTML = "휴식";
+        bettingScreen.style.borderColor = "#c7c7c7";
+        bettingScreen.style.boxShadow = "0 0 0 1px #c7c7c7 inset";
+        bettingScreen.style.color = "#c7c7c7";
 
     }
 
-    if(playerScore >= limit){
-        bettingScreen.innerHTML = "배팅금지";
+    if ( playerScore >= limit ) {
+
+        alert( "마틴에 도달되었습니다. 게임을 다시 시작합니다." );
+        gameReset();
+
     }
 
-    // document.querySelector("#playerScore").innerHTML = playerScore;
-    // document.querySelector("#bankerScore").innerHTML = bankerScore;
+    document.querySelector("#playerScore").innerHTML = playerScore;
+    document.querySelector("#bankerScore").innerHTML = bankerScore;
 
 })
+
+function manual (){
+
+    document.querySelector("#manual").addEventListener("click", function(){
+        document.querySelector("#gameMenu").style.display = "block";
+    })
+    document.querySelector("#gameMenuCloseBtn").addEventListener("click", function(){
+        document.querySelector("#gameMenu").style.display = "none";
+    })
+
+}
+manual ();
+
+function nightMode(){
+
+    const nightModeBtn = document.querySelector("#nightMode")
+
+    nightModeBtn.addEventListener("click", function(){
+        nightModeBtn.classList.toggle("active");
+        document.querySelector("html").classList.toggle("dark");
+        document.querySelector("#gameMenu").classList.toggle("dark");
+    })
+}
+nightMode()
